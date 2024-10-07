4ab583c92474619c69f53e06a19560b5eb24c34f
menneni <risingstar.teja@gmail.com>
2024-10-07 13:56:01 +0530

    add infinite loop

--- app/server.go
+++ app/server.go
@@ -18,20 +18,22 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	_, err = c.Read(make([]byte, 1024))
-	if err != nil {
-		fmt.Println("Error reading connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer c.Close()
-	_, err = c.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-		os.Exit(1)
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		_, err = c.Read(make([]byte, 1024))
+		if err != nil {
+			fmt.Println("Error reading connection: ", err.Error())
+			os.Exit(1)
+		}
+		defer c.Close()
+		_, err = c.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 }
